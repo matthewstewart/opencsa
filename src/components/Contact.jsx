c0fc4da8c535a8dde584e22602caba4a4274a4d5
TS22082 <ts22082@gmail.com>
2018-10-07 08:38:29 -0700

    add nav and contact components

---
+++ src/components/Contact.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Contact extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h3>Contact Works</h3>
+      </div>
+    );
+  }
+}
+
+export default Contact;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Nav extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h3>Nav works</h3>
+      </div>
+    );
+  }
+}
+
+export default Nav;
